b46617acf52226ebc91827f4619dcd27ccdf9a95
lijinyi_vendor <lijinyi_vendor@sensetime.com>
2020-10-21 11:42:08 +0800

    feat: 链接mongo数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-blog'),
+    UserModule
+  ],
   controllers: [],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,24 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import {User} from './user.schema';
 
 @Controller('user')
 @ApiTags('用户相关')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
   @ApiOperation({ summary: '获取个人信息' })
-  getUserInfo() {
-    return [];
+  getUserInfo(): Promise<User[]> {
+    return this.userService.findAll();
   }
 
   @Post()
   @ApiOperation({ summary: '注册新用户' })
-  createUser(@Body() body: CreateUserDto, @Query() query, @Param() params) {
-    return {
-      success: true,
-    };
+  async createUser(@Body() body: CreateUserDto) {
+    await this.userService.create(body)
   }
 
   @Get(':id')
